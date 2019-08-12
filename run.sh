7621be7862b9218eb1aecebd73c30160c3a4211a
BIBO <49994376+BIBOLV@users.noreply.github.com>
2019-08-12 16:36:04 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo hciconfig hci0 reset
 sleep 2
-python $DIR/ble-watch-read.py <<WATCH_BLE_ADDRESS>> <<WATCH_FRIENDLY_NAME>>
+python $DIR/ble-watch-read.py 00:00:00:00:00:00 hr_watch_mom
